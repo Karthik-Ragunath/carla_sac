f03571ed16be6f92ba187b53952e8cbc79c10fad
Karthik Ragunath Ananda Kumar <a.karthikragunath@gmail.com>
2022-11-20 05:13:55 -0600

    starting training 4

--- env_config.py
+++ env_config.py
@@ -69,5 +69,5 @@ EnvConfig = {
     'test_env_params': test_env_params,
 
     # training context
-    'train_context': "rgb_actor_critic_modified_v3"
+    'train_context': "rgb_actor_critic_modified_v4"
 }
