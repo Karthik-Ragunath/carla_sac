7765be96576c296b7548714cb9586627106255e6
Karthik Ragunath Ananda Kumar <a.karthikragunath@gmail.com>
2023-01-20 17:36:50 -0600

    dev

---
+++ carla_ppo_train.py
@@ -0,0 +1,87 @@
+import argparse
+import numpy as np
+import torch
+from utils_ppo import DrawLine
+import logging
+from torch.utils.tensorboard import SummaryWriter
+from torch_base.torch_agent_ppo import Agent
+from ppo_env import Env
+
+parser = argparse.ArgumentParser(description='Train a PPO agent for the CarRacing-v0')
+parser.add_argument('--gamma', type=float, default=0.99, metavar='G', help='discount factor (default: 0.99)')
+parser.add_argument('--action-repeat', type=int, default=8, metavar='N', help='repeat action in N frames (default: 8)')
+parser.add_argument('--img-stack', type=int, default=4, metavar='N', help='stack N image in a state (default: 4)')
+parser.add_argument('--seed', type=int, default=0, metavar='N', help='random seed (default: 0)')
+parser.add_argument('--render', action='store_true', help='render the environment')
+parser.add_argument('--vis', action='store_true', help='use visdom')
+parser.add_argument(
+    '--log-interval', type=int, default=10, metavar='N', help='interval between training status logs (default: 10)')
+parser.add_argument("--device_id", "-dev", type=int, default=0, required=False)
+parser.add_argument("--log_seed", type=int, default=0, required=False)
+args = parser.parse_args()
+
+use_cuda = torch.cuda.is_available()
+device = torch.device(f"cuda:{args.device_id}" if use_cuda else "cpu")
+torch.manual_seed(args.seed)
+if use_cuda:
+    torch.cuda.manual_seed(args.seed)
+
+LOGGER= logging.getLogger()
+LOGGER.setLevel(logging.DEBUG) # or whatever
+handler = logging.FileHandler(f"ppo_logger_{args.log_seed}.log", 'w', 'utf-8')
+formatter = logging.Formatter('%(name)s %(message)s')
+handler.setFormatter(formatter)
+LOGGER.addHandler(handler)
+
+if __name__ == '__main__':
+    writer = SummaryWriter()
+    agent = Agent()
+    env = Env()
+    if args.vis:
+        draw_reward = DrawLine(env="car", title="PPO", xlabel="Episode", ylabel="Moving averaged episode reward")
+
+    training_records = []
+    running_score = 0
+    state = env.reset()
+    best_episode_reward = 0
+    best_episode_running_score = 0
+    LOGGER.info("start training")
+    for i_ep in range(200000):
+        score = 0
+        state = env.reset()
+
+        for t in range(1000):
+            action, a_logp = agent.select_action(state)
+            state_, reward, done, die = env.step(action * np.array([2., 1., 1.]) + np.array([-1., 0., 0.]))
+            if args.render:
+                env.render()
+            if agent.store((state, action, a_logp, reward, state_)):
+                print('updating')
+                agent.update()
+            score += reward
+            state = state_
+            if done or die:
+                break
+        running_score = running_score * 0.99 + score * 0.01
+
+        if score > best_episode_reward:
+            best_episode_reward = score
+            agent.save_checkpoint_reward(i_ep)
+
+        if running_score > best_episode_running_score:
+            best_episode_running_score = running_score
+            agent.save_checkpoint_running_score(i_ep)
+
+        LOGGER.info('Ep {}\tLast score: {:.2f}\tMoving average score: {:.2f}'.format(i_ep, score, running_score))
+
+        writer.add_scalar('train_reward_score', score, i_ep)
+        writer.add_scalar('train_reward_running_score', running_score, i_ep)
+
+        if i_ep % args.log_interval == 0:
+            if args.vis:
+                draw_reward(xdata=i_ep, ydata=running_score)
+            print('Ep {}\tLast score: {:.2f}\tMoving average score: {:.2f}'.format(i_ep, score, running_score))
+            agent.save_param()
+        if running_score > env.reward_threshold:
+            print("Solved! Running reward is now {} and the last episode runs to {}!".format(running_score, score))
+            break
---
+++ ppo_env.py
@@ -0,0 +1,75 @@
+import gym
+import numpy as np
+
+class Env():
+    """
+    Environment wrapper for CarRacing 
+    """
+
+    # TODO: added args to __init__
+    def __init__(self, args):
+        self.args = args
+        if self.args.render:
+            self.env = gym.make('CarRacing-v2', render_mode="human")
+        else:
+            self.env = gym.make('CarRacing-v2')
+        # self.env.seed(self.args.seed)
+        self.reward_threshold = self.env.spec.reward_threshold
+
+    def reset(self):
+        self.counter = 0
+        self.av_r = self.reward_memory()
+
+        self.die = False
+        img_rgb = self.env.reset()[0]
+        img_gray = self.rgb2gray(img_rgb)
+        self.stack = [img_gray] * self.args.img_stack  # four frames for decision
+        return np.array(self.stack)
+
+    def step(self, action):
+        total_reward = 0
+        for i in range(self.args.action_repeat):
+            img_rgb, reward, die, _, _ = self.env.step(action)
+            # don't penalize "die state"
+            if die:
+                reward += 100
+            # green penalty
+            if np.mean(img_rgb[:, :, 1]) > 185.0:
+                reward -= 0.05
+            total_reward += reward
+            # if no reward recently, end the episode
+            done = True if self.av_r(reward) <= -0.1 else False
+            if done or die:
+                break
+        img_gray = self.rgb2gray(img_rgb)
+        self.stack.pop(0)
+        self.stack.append(img_gray)
+        assert len(self.stack) == self.args.img_stack
+        return np.array(self.stack), total_reward, done, die
+
+    def render(self, *arg):
+        self.env.render(*arg)
+
+    @staticmethod
+    def rgb2gray(rgb, norm=True):
+        # rgb image -> gray [0, 1]
+        gray = np.dot(rgb[..., :], [0.299, 0.587, 0.114])
+        if norm:
+            # normalize
+            gray = gray / 128. - 1.
+        return gray
+
+    @staticmethod
+    def reward_memory():
+        # record reward for last 100 steps
+        count = 0
+        length = 100
+        history = np.zeros(length)
+
+        def memory(reward):
+            nonlocal count
+            history[count] = reward
+            count = (count + 1) % length
+            return np.mean(history)
+
+        return memory
