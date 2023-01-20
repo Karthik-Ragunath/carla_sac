ee405eed71f8824e03a81491990c18baee0301f8
Karthik Ragunath Ananda Kumar <a.karthikragunath@gmail.com>
2023-01-20 17:01:29 -0600

    dev

--- torch_base/torch_agent_ppo.py
+++ torch_base/torch_agent_ppo.py
@@ -1,4 +1,9 @@
 from .torch_model_ppo import Net
+import torch
+import numpy as np
+import torch.optim as optim
+from torch.distributions import Beta
+from torch.utils.data.sampler import BatchSampler, SubsetRandomSampler
 
 class Agent():
     """
@@ -9,16 +14,26 @@ class Agent():
     ppo_epoch = 10
     buffer_capacity, batch_size = 2000, 128
 
-    def __init__(self):
+    # TODO: added device, args as an argument
+    def __init__(self, device, args):
+        self.device = device
+        self.transition_type = np.dtype(
+            [
+                ('s', np.float64, 
+                (args.img_stack, 96, 96)), 
+                ('a', np.float64, (3,)), 
+                ('a_logp', np.float64),
+                ('r', np.float64), ('s_', np.float64, (args.img_stack, 96, 96))
+            ]
+        )
         self.training_step = 0
-        self.net = Net().double().to(device)
-        self.buffer = np.empty(self.buffer_capacity, dtype=transition)
+        self.net = Net().double().to(self.device)
+        self.buffer = np.empty(self.buffer_capacity, dtype=self.transition_type)
         self.counter = 0
-
         self.optimizer = optim.Adam(self.net.parameters(), lr=1e-3)
 
     def select_action(self, state):
-        state = torch.from_numpy(state).double().to(device).unsqueeze(0)
+        state = torch.from_numpy(state).double().to(self.device).unsqueeze(0)
         with torch.no_grad():
             alpha, beta = self.net(state)[0]
         dist = Beta(alpha, beta)
@@ -30,7 +45,13 @@ class Agent():
         return action, a_logp
 
     def save_param(self):
-        torch.save(self.net.state_dict(), 'param/ppo_net_params_trained.pkl')
+        torch.save(self.net.state_dict(), 'param/ppo_net_params_model_trained.pkl')
+    
+    def save_checkpoint_reward(self, episode):
+        torch.save(self.net.state_dict(), f"param/reward_checkpoint_{episode}.pkl")
+
+    def save_checkpoint_running_score(self, episode):
+        torch.save(self.net.state_dict(), f"param/run_score_checkpoint_{episode}.pkl")
 
     def store(self, transition):
         self.buffer[self.counter] = transition
@@ -41,15 +62,16 @@ class Agent():
         else:
             return False
 
-    def update(self):
+    # TODO: send args as a param
+    def update(self, args):
         self.training_step += 1
 
-        s = torch.tensor(self.buffer['s'], dtype=torch.double).to(device)
-        a = torch.tensor(self.buffer['a'], dtype=torch.double).to(device)
-        r = torch.tensor(self.buffer['r'], dtype=torch.double).to(device).view(-1, 1)
-        s_ = torch.tensor(self.buffer['s_'], dtype=torch.double).to(device)
+        s = torch.tensor(self.buffer['s'], dtype=torch.double).to(self.device)
+        a = torch.tensor(self.buffer['a'], dtype=torch.double).to(self.device)
+        r = torch.tensor(self.buffer['r'], dtype=torch.double).to(self.device).view(-1, 1)
+        s_ = torch.tensor(self.buffer['s_'], dtype=torch.double).to(self.device)
 
-        old_a_logp = torch.tensor(self.buffer['a_logp'], dtype=torch.double).to(device).view(-1, 1)
+        old_a_logp = torch.tensor(self.buffer['a_logp'], dtype=torch.double).to(self.device).view(-1, 1)
 
         with torch.no_grad():
             target_v = r + args.gamma * self.net(s_)[1]
