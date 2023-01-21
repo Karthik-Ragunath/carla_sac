b2b016364d1b55d1d6bf5d90d86addf008cd7bd2
Karthik Ragunath Ananda Kumar <a.karthikragunath@gmail.com>
2023-01-21 13:08:42 -0600

    dev

--- ppo_env.py
+++ ppo_env.py
@@ -1,5 +1,8 @@
 import gym
 import numpy as np
+from torch_base.detect_bounding_boxes import DetectBoundingBox
+import matplotlib.pyplot as plt
+
 
 class Env():
     """
@@ -72,3 +75,88 @@ class Env():
             return np.mean(history)
 
         return memory
+
+class CarlaEnv(object):
+    def __init__(self, env_name, params, context):
+        class ActionSpace(object):
+            def __init__(self,
+                         action_space=None,
+                         low=None,
+                         high=None,
+                         shape=None,
+                         n=None):
+                self.action_space = action_space
+                self.low = low
+                self.high = high
+                self.shape = shape
+                self.n = n
+
+            def sample(self):
+                return self.action_space.sample()
+        self.env = gym.make(env_name, params=params)
+        self._max_episode_steps = int(params['max_time_episode'])
+        self.action_space = ActionSpace(
+            self.env.action_space, self.env.action_space.low,
+            self.env.action_space.high, self.env.action_space.shape)
+        self.save_episode = False
+        self.episode_num = -1
+        self.eval_episode_num = 0
+        self.train_vis_dir = context + '_train'
+        self.valid_vis_dir = context + '_valid'
+
+    def to_bgra_array(self, image):
+        """Convert a CARLA raw image to a BGRA numpy array."""
+        array = np.frombuffer(image.raw_data, dtype=np.dtype("uint8"))
+        array = np.reshape(array, (image.height, image.width, 4))
+        return array
+
+    def to_rgb_array(self, image):
+        """Convert a CARLA raw image to a RGB numpy array."""
+        array = self.to_bgra_array(image)
+        # Convert BGRA to RGB.
+        array = array[:, :, :3]
+        array = array[:, :, ::-1]
+        return array
+
+    def reset(self):
+        current_image = self.env.reset()
+        bounded_image = None
+        numpy_rgb_image = None
+        if current_image:
+            numpy_rgb_image = self.to_rgb_array(current_image)
+            faster_rcnn_obj = DetectBoundingBox(numpy_rgb_image, str(current_image.frame) + '.png')
+            bounded_image = faster_rcnn_obj.detect_bounding_boxes()
+            if self.save_episode:
+                fig = plt.figure()
+                plt.imshow(bounded_image)
+                plt.savefig(os.path.join(os.getcwd(), self.train_vis_dir, str(self.episode_num), (str(current_image.frame) + '.png')))
+                plt.close(fig)
+        else:
+            print("NO IMAGE DETECTED FOR NOW IN RESET")
+        return numpy_rgb_image, bounded_image
+
+    def step(self, action, is_validation=False):
+        assert np.all(((action<=1.0 + 1e-3), (action>=-1.0 - 1e-3))), \
+            'the action should be in range [-1.0, 1.0]'
+        # mapped_action = [-2, -2] * ((action + 1) * 2) = action * 2
+        mapped_action = self.action_space.low + (action - (-1.0)) * (
+            (self.action_space.high - self.action_space.low) / 2.0)
+        mapped_action = np.clip(mapped_action, self.action_space.low, self.action_space.high)
+        action_out, current_image = self.env.step(mapped_action)
+        bounded_image = None
+        numpy_rgb_image = None
+        if current_image:
+            numpy_rgb_image = self.to_rgb_array(current_image)
+            faster_rcnn_obj = DetectBoundingBox(numpy_rgb_image, str(current_image.frame) + '.png')
+            bounded_image = faster_rcnn_obj.detect_bounding_boxes()
+            if self.save_episode or is_validation:
+                fig = plt.figure()
+                plt.imshow(bounded_image)
+                if is_validation:
+                    plt.savefig(os.path.join(os.getcwd(), self.valid_vis_dir, str(self.eval_episode_num), (str(current_image.frame) + '.png')))
+                else:
+                    plt.savefig(os.path.join(os.getcwd(), self.train_vis_dir, str(self.episode_num), (str(current_image.frame) + '.png')))
+                plt.close(fig)
+        else:
+            print("NO IMAGE DETECTED FOR NOW IN STEP")
+        return action_out, numpy_rgb_image, bounded_image
