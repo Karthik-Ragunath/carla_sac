f109cb80ccbd0de0de16f1d0832c371afc94e64d
Karthik-Ragunath <a.karthikragunath@gmail.com>
2023-01-29 03:22:01 -0600

    dev

--- env_config.py
+++ env_config.py
@@ -5,8 +5,7 @@ from copy import deepcopy
 #      Lane         250
 #      Long         200
 TASK_MODE = 'Lane'
-MAX_EPISODE_STEPS = 1000
-# MAX_EPISODE_STEPS = 475
+MAX_EPISODE_STEPS = 3000
 OPEN_AI_IMPLEMENTATION = False
 
 params = {
