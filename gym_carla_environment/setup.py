1976bfbe35a001169a7bd981a9d6f59a58aa8c6f
Karthik Ragunath Ananda Kumar <a.karthikragunath@gmail.com>
2023-02-04 18:33:05 -0600

    creating new gym environment

--- gym_carla_environment/setup.py
+++ gym_carla_environment/setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup
 
-setup(name='gym_carla', version='0.1', install_requires=['gym', 'pygame'])
+setup(name='gym_carla_environment', version='0.1', install_requires=['gym', 'pygame'])
