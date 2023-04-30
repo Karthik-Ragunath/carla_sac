59d9c9b42aafa4e564f0e9fa41bbd828ce603e49
Karthik Ragunath Ananda Kumar <a.karthikragunath@gmail.com>
2023-04-30 03:01:28 -0500

    dev imitation model

---
+++ imitation_train.py
@@ -0,0 +1,144 @@
+# from customModel import CNNClassifier, save_model, load_model
+from torch_base.torch_agent_inference_ppo import ImitationLearningAgent
+from imitation_utils import accuracy, load_data
+import torch
+from torchvision import models
+from torch.utils.tensorboard import SummaryWriter
+import os
+import logging
+from env_config import EnvConfig
+import argparse
+
+parser = argparse.ArgumentParser(description='Train a PPO agent for the CarRacing-v0')
+
+parser.add_argument('-l', '--lrate', type=float, default=0.001)
+parser.add_argument('-e', '--epochs', type=int, default=450)
+parser.add_argument('-t', '--tbdir', type=str, default="")
+parser.add_argument('--img-stack', type=int, default=4, metavar='N', help='stack N image in a state (default: 4)')
+# parser.add_argument('--gamma', type=float, default=0.99, metavar='G', help='discount factor (default: 0.99)')
+# parser.add_argument('--action-repeat', type=int, default=8, metavar='N', help='repeat action in N frames (default: 8)')
+# parser.add_argument('--img-stack', type=int, default=4, metavar='N', help='stack N image in a state (default: 4)')
+parser.add_argument('--seed', type=int, default=0, metavar='N', help='random seed (default: 0)')
+# parser.add_argument('--render', action='store_true', help='render the environment')
+parser.add_argument("--device_id", "-dev", type=int, default=0, required=False)
+parser.add_argument("--log_seed", type=str, default=0, required=False)
+# parser.add_argument("--num_episodes", type=int, default=1, required=False)
+parser.add_argument("--context", type=str, default='imitation_0', required=False)
+# parser.add_argument("--num_steps_per_episode", type=int, default=250, required=False)
+parser.add_argument("--load_context", type=str, required=False)
+args = parser.parse_args()
+
+use_cuda = torch.cuda.is_available()
+device = torch.device(f"cuda:{args.device_id}" if use_cuda else "cpu")
+torch.manual_seed(args.seed)
+if use_cuda:
+    torch.cuda.manual_seed(args.seed)
+
+LOGGER= logging.getLogger()
+LOGGER.setLevel(logging.DEBUG) # or whatever
+handler = logging.FileHandler(f"ppo_logger_imitation_{args.log_seed}.log", 'w', 'utf-8')
+formatter = logging.Formatter('%(name)s %(message)s')
+handler.setFormatter(formatter)
+LOGGER.addHandler(handler)
+
+if __name__ == "__main__":
+    """Main function."""
+    checkpoints_save_dir = 'params_' + args.context
+    if not os.path.exists(checkpoints_save_dir):
+        os.makedirs(checkpoints_save_dir)
+
+    agent = ImitationLearningAgent(device=device, env_params=EnvConfig['imitation_env_params'], context=args.context, args=args)
+    
+    dirname = os.path.join("runs", args.tbdir)
+    tb = SummaryWriter(log_dir=dirname)
+
+    # --- Initializations ---
+    pretrained_epochs = agent.load_param(load_context=args.load_context)
+    # model = CNNClassifier() #models.resnet18(pretrained=True)
+    # model = load_model()
+
+    '''
+    # # --- Freeze layers and replace FC layer ---
+    # for name, param in model.named_parameters():
+    #     if param.requires_grad:
+    #         if "fc" not in name:
+    #             param.requires_grad = False
+    # model.fc = torch.nn.Linear(512, 3)
+    '''
+
+    # Potential GPU optimization.
+    # device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    # model.to(device)
+
+    optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, agent.net.parameters()), lr=args.lrate)
+    criterion = torch.nn.MSELoss()
+    train_loader = load_data("/home/kxa200005/data/train")
+    validation_loader = load_data("/home/kxa200005/data/valid")
+
+    # --- SGD Iterations ---
+    for epoch in range(pretrained_epochs + 1, args.epochs):
+        
+        print("Starting Epoch: ", epoch)
+        if (epoch % 10 == 0 and epoch != 0):
+            agent.save_param(epoch=epoch)
+
+        # Per epoch train loop.
+        agent.net.train()
+        for _, (rgb_input, sem_input, yhat) in enumerate(train_loader):
+            yhat = yhat.view(-1, 3, 1)
+            yhat = yhat.cuda()
+            optimizer.zero_grad()
+            # sem_input.to(device)
+            # ypred = model(rgb_input.cuda())
+            ypred = agent.select_action(rgb_input)
+            ypred = ypred.view(-1, 3, 1)
+            loss = criterion(ypred, yhat)
+            loss.backward()
+            optimizer.step()
+
+            # Record training loss and accuracy
+            tb.add_scalar("Train Loss", loss)
+            steer, throttle, brake, average = accuracy(ypred, yhat)
+            tb.add_scalar("Train Accuracy", average)
+
+            tb.add_scalar("Steer Accuracy", steer)
+            tb.add_scalar("Throttle Accuracy", throttle)
+            tb.add_scalar("Brake Accuracy", brake)
+            LOGGER.info('Epoch {}\t Train Accuracy: {:.2f}\t Steer Accuracy: {:.2f}\t Throttle Accuracy: {:.2f} \t Brake Accuracy: {}'.format(epoch, average, steer, throttle, brake))
+        
+        # After each train epoch, do validation before starting next train epoch.
+        agent.net.eval()
+        for _, (rgb_input, sem_input, yhat) in enumerate(validation_loader):
+            yhat = yhat.view(-1, 3, 1)
+            yhat = yhat.cuda()
+            with torch.no_grad():
+                # sem_input.to(device)
+                # ypred = model(sem_input.cuda())
+                ypred = agent.select_action(rgb_input)
+                ypred = ypred.view(-1, 3, 1)
+                loss = criterion(ypred, yhat)
+
+            # Record validation loss and accuracy
+            tb.add_scalar("Validation Loss", loss)
+            steer, throttle, brake, average = accuracy(ypred, yhat)
+            tb.add_scalar("Validation Accuracy", average)
+            # tb.add_scalar("Steer Accuracy", steer, epoch)
+            # tb.add_scalar("Throttle Accuracy", throttle, epoch)
+            # tb.add_scalar("Brake Accuracy", brake, epoch)
+    '''
+    save_model(model)
+    '''
+    agent.save_param(epoch=epoch)
+
+'''
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-l', '--lrate', type=float, default=0.001)
+    parser.add_argument('-e', '--epochs', type=int, default=450)
+    parser.add_argument('-t', '--tbdir', type=str, default="")
+
+    args = parser.parse_args()
+    train(args)
+'''
---
+++ imitation_utils.py
@@ -0,0 +1,120 @@
+"""
+Handles dataset organization for dataloader.
+Additionally used as a utility to check model accuracy.
+"""
+
+import csv
+import os
+import torch
+
+from PIL import Image, ImageOps
+
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+import numpy as np
+
+class CarlaDataset(Dataset):
+    """
+    Classification data from CarlaDataset.
+    Represented as tuples of 3 x 150 x 200 images and their vectors of data/labels
+    """
+    def __init__(self, dataset_path):
+
+        self.csv_tuples = []
+        self.dataset_path = dataset_path
+        self.transform = transforms.Compose([
+            transforms.Resize(96),
+            # transforms.CenterCrop(224), 
+            transforms.ToTensor()])
+
+        # Extract data from csv.
+        labels_path = os.path.join(dataset_path, "labels.csv")
+        with open(labels_path, "r", encoding="utf-8") as file:
+            reader = csv.reader(file)
+            for row in reader:
+                self.csv_tuples.append((row[0], row[1], row[2], row[3], row[4])) 
+                # 0 is rgb fname, 1 is sem name, 2 is data/labels vector
+
+        # Cut out the csv headers from extracted data.
+        self.csv_tuples = self.csv_tuples[1:]
+
+
+    def __len__(self):
+        """
+        Your code here
+        returns length of dataset.
+        """
+        return len(self.csv_tuples)
+
+    @staticmethod
+    def rgb2gray(rgb, norm=True):
+        # rgb image -> gray [0, 1]
+        np_rgb = np.array(rgb)
+        gray = np.dot(np_rgb[..., :], [0.299, 0.587, 0.114])
+        if norm:
+            # normalize
+            gray = gray / 128. - 1.
+        return gray
+
+    def __getitem__(self, idx):
+        """
+        Your code here
+        return a tuple: img, label
+        """
+
+        # All pairs of image and label are added to csv_tuples string list.
+        # Grab the data vector from 2nd index, get only labels and change to floats
+        data = [float(self.csv_tuples[idx][2]),float(self.csv_tuples[idx][3]), float(self.csv_tuples[idx][4])]
+
+        # border = (0, 150, 0, 0) # cut 0 from left, 30 from top, right, bottom
+
+        # Rgb image as a tensor
+        gray_image = Image.open(os.path.join(self.dataset_path, self.csv_tuples[idx][0])).convert('L')
+        # gray_image = self.rgb2gray(rgb_image)
+        # # # rgb_image = ImageOps.crop(rgb_image, border)
+        # # rgb_tensor = self.transform(gray_image)
+        # gray_tensor = torch.from_numpy(gray_image).double()
+        gray_tensor = self.transform(gray_image)
+
+        # Sem image as an input tensor
+        sem_image = Image.open(os.path.join(self.dataset_path, self.csv_tuples[idx][1]))
+        # sem_image = ImageOps.crop(sem_image, border)
+        sem_tensor = self.transform(sem_image)
+
+        # return rgb_tensor, sem_tensor, torch.tensor(data)
+        return gray_tensor, sem_tensor, torch.tensor(data)
+
+
+def load_data(dataset_path, num_workers=0, batch_size=32):
+    """
+    Driver function to create dataset and return constructed dataloader.
+    """
+    dataset = CarlaDataset(dataset_path)
+    return DataLoader(dataset, num_workers=num_workers, batch_size=batch_size, shuffle=True, drop_last=False)
+
+
+def accuracy(yhat, y): # yhat is not labels it is prediction
+    """
+    Returns accuracy between true labels and predictions.
+    """
+    def rmse(y_tensor, yhat_tensor):
+        return torch.sqrt(torch.mean(torch.pow((y_tensor - yhat_tensor), 2)))
+
+    steer_rmse = throttle_rmse = brake_rmse = 0
+    for j in range(len(y)):
+        steer_rmse += rmse(y[j, 0], yhat[j, 0])
+        throttle_rmse += rmse(y[j, 1], yhat[j, 1])
+        brake_rmse += rmse(y[j, 2], yhat[j, 2])
+
+    steer_rmse /= len(y)
+    throttle_rmse /= len(y)
+    brake_rmse /= len(y)
+
+    # accuracy_steer = round(1 - steer_rmse.item(), 3)
+    # accuracy_throttle = round(1 - throttle_rmse.item(), 3)
+    # accuracy_brake = round(1 - brake_rmse.item(), 3)
+    accuracy_steer = round(steer_rmse.item(), 3)
+    accuracy_throttle = round(throttle_rmse.item(), 3)
+    accuracy_brake = round(brake_rmse.item(), 3)
+    accuracy_avg = round((accuracy_steer + accuracy_throttle + accuracy_brake) / 3, 3)
+    return accuracy_steer, accuracy_throttle, accuracy_brake, accuracy_avg
